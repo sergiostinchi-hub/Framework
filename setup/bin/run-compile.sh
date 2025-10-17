199336ba33b72b2273be5462fdc4f62203e00d8a
sergiostinchi <sergio.stinchi@gmail.com>
2025-10-17 14:57:31 +0200

    Update run-compile.sh

--- setup/bin/run-compile.sh
+++ setup/bin/run-compile.sh
@@ -1,7 +1,30 @@
-#!/bin/bash
+@echo off
+setlocal
+
 UPDATE_VER=9.1
 FULL_VER=4.1
+PRJ_PATH=C:\Users\sstinchi\Documents\repos\Framework
 
-zip -r /prod/IBM/software/framework_beta
+echo Quale archivio vuoi creare? (UPDATE/FULL)
+set /p risposta=
 
-zip -r /home/admin/download.zip 
+IF /I "%risposta%"=="UDATE" (
+    zip -r Framework_update_sogei_V%{UPDATE_VER}.zip %{PRJ_PATH} -x  %{PRJ_PATH}/ant-contrib %{PRJ_PATH}/apache-ant  %{PRJ_PATH}/apache-ant %{PRJ_PATH}/dist %{PRJ_PATH}/download %{PRJ_PATH}/jdk1.8.0_152 %{PRJ_PATH}/Manual %{PRJ_PATH}/
+) 
+ELSE IF /I "%risposta%"=="FULL"(
+    zip -r Framework_FULL_SOGEI_V%{FULL_VER}.zip %{PRJ_PATH}   
+}
+ELSE(    
+    echo Operazione annullata.
+)
+
+endlocal
+pause
+
+
+
+
+
+
+
+
